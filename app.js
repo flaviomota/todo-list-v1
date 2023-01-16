185a3fd449182574d9f90017979cd37e4f9ca7d0
Flávio Mota <fmota@student.42lisboa.com>
2023-01-16 16:22:45 +0000

    templating

--- app.js
+++ app.js
@@ -5,8 +5,42 @@ const port = 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-	res.send('Hello');
+	
+	let today = new Date();
+	let currentDay = today.getDay();
+	let day = "";
+
+	switch (currentDay) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		default:
+			console.log(`Error: current day is equal to: ${currentDay}`);	
+	}
+
+	res.render("list", {kindOfDay: day});
+
 });
 
 app.listen(port, function(){
