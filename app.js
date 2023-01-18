5d9a92653458761595e3d5e2264686943a14e1be
Flávio Mota <fmota@student.42lisboa.com>
2023-01-18 18:04:27 +0000

    refactoring

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const port = 3000;
 
@@ -14,15 +15,7 @@ app.use(express.static("public"));
 
 app.get('/', function(req, res){
 	
-	let today = new Date();
-
-	let options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long"
-	};
-
-	let day = today.toLocaleDateString("en-US", options);
+	const day = date.getDate();
 
 	res.render("list", {kindOfDay: day, newListItems: items});
 
